7e331ad711c4f73f16aa9e5f754dcf2b0a6042ef
zhuojianhai <fdsa1234>
2020-08-02 16:25:42 +0800

    数据结构学习

---
+++ javademo/src/main/java/com/zjh/javademo/datastructor/tree/TestTree.java
@@ -0,0 +1,10 @@
+package com.zjh.javademo.datastructor.tree;
+
+import com.zjh.javademo.datastructor.tree.bean.Student;
+
+public class TestTree {
+    public static void main(String[] args) {
+        BinarySearchTree<Student> binarySearchTree = new BinarySearchTree<>();
+
+    }
+}
